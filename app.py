36566e28fc37dcaa27a7ee6bd12e239a6f2b6ddd
M0reti <00001100109675sp@al.educacao.sp.gov.br>
2025-08-05 08:37:16 -0300

    Criando projeto no streamlit

---
+++ app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import time
+from controllers.load_usuarios import load_usuarios  
+
+st.title("Projeto streamlit")
+
+if "email" not in st.session_state:
+    st.session_state.email = None
+
+if "nome" not in st.session_state:
+    st.session_state.nome = None
+
+if "contador" not in st.session_state:
+    st.session_state.contador = 0
+
+def login():
+    usuarios = load_usuarios()
+    email = st.text_input("Email", placeholder="Email")
+    senha = st.text_input("Senha", placeholder="senha", type="password")
+    login = st.button("login")
+
+    if login:
+        for user in usuarios:
+            if user["email"] == email and user["senha"] == senha:
+                st.session_state.email = user["email"]
+                st.session_state.nome = user["nome"]
+                st.success("login efetuado com sucesso!")
+                time.sleep(3)
+                st.rerun()
+        else:
+            st.error("Email ou senha incorretos")
+
+def logout():
+    if st.button("Logout"):
+        st.session_state.clear()
+        st.success("Finalizando o Sistema")
+        time.sleep(3)
+
+@st.dialog("Formulario de cadastro de aluno")
+def cadastrar_aluno():
+    nome_aluno = st.text_input("Nome do Aluno", placeholder= "Nome do Aluno")
+    email_aluno = st.text_input("Email do Aluno", placeholder= "Email do Aluno")
+    cpf_aluno = st.text_input("CPF do Aluno", placeholder= "CPF do Aluno")
+    datadenasci_aluno = st.date_input("Data do Aluno")
+    telefone_aluno = st.text_input("Telefone do Aluno", placeholder= "Telefone do Aluno")
+
+btn_cadastrar = st.button(f"Cadastrar")
+
+
+def main_page():
+    tabs = st.tabs(["Dashboard", "Cadastro", "Logout"])
+    nome = st.session_state.nome
+
+    with tabs[0]:
+        st.subheader("Dashboard")
+        st.write(f"**Usuario Logado:** {nome}")
+
+    with tabs[1]:
+        st.subheader("Cadastro")
+        if st.button("Abrir formulario de cadastro"):
+            cadastrar_aluno()
+
+    with tabs[2]:
+        st.subheader("Logout")
+        logout()
+   
+  
+if st.session_state.email:
+    main_page()
+else:
+    login()
+
+# if st.button("Adicionar"):
+#     st.session_state.contador += 1
+
+# if st.button("Diminuir"):
+#     if st.session_state.contador > 0:
+#         st.session_state.contador -= 1
+
+# st.write(st.session_state)
